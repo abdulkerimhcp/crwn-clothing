40e342904182bf1014d7aded79a58458c6791066
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-09-03 17:46:01 +0300

    updating our app with a shop page, collection preview component adn collection item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./components/pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./components/pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/pages/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import ColletionPreview from "../../preview-collection/collection-preview.component";
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <ColletionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
