7318d0846eab02a3840b925809e79ec393bdee18
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-09-03 18:25:36 +0300

    addingg header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,10 +46,7 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem
-            key={id}
-            {...otherSectionProps}
-          />
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
